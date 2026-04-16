81e53e39177a0ce4c56354bbe78833fba0929bda
DiegoF1906 <est.diegof.hernand1@unimilitar.edu.co>
2026-04-15 21:34:09 -0500

    Definicion del viewport

--- script.js
+++ script.js
@@ -2,4 +2,10 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-console.log("Proyecto inicializado.");
+// Definición de los límites del Viewport
+const X_MIN = 0;
+const Y_MIN = 0;
+const X_MAX = 250;
+const Y_MAX = 220;
+
+console.log(`Viewport definido: X(${X_MIN}, ${X_MAX}), Y(${Y_MIN}, ${Y_MAX})`);
